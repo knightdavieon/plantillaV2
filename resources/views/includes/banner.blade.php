a9a2102cfbaae55f259a13cd3067d66f5b79ef46
John Dave Espinosa <espinosa.johndave27@gmail.com>
2018-10-19 14:13:18 +0800

    carousel banner

--- resources/views/includes/banner.blade.php
+++ resources/views/includes/banner.blade.php
@@ -1,3 +1,14 @@
-<div class="container container-fluid rectangle-img">
-	<!-- <img src="{{ asset( 'images/biography.jpg' )}}" class="img-fluid rectangle-img" /> -->
+
+<div id="carousel" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    	<div class="carousel-caption d-none d-md-block">
+			<h5>TEST TEST</h5>
+		    <p>...</p>
+	  	</div>
+	  	<div class="overlay"></div>
+      <img class="w-100 h-50" src="{{ asset( 'images/biography.jpg' )}}" alt="First slide">
+       	
+    </div>
+  </div>
 </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="resources/admindesign/image/png" sizes="16x16" href="#">
+    <link rel="icon" sizes="16x16" href="{{ asset( 'images/lalawiganngzambales.png' ) }}">
 	<title>Intergalactic President Space Earth Ambassador</title>
 	<link rel="stylesheet" href="/css/app.css">
 	<link rel="stylesheet" href="/css/customscss.css">
