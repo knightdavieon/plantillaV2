fff39881ae1b25c379366caed3616e40cee351ea
John Dave Espinosa <espinosa.johndave27@gmail.com>
2018-10-19 14:54:09 +0800

    update center card banner

--- resources/views/includes/banner.blade.php
+++ resources/views/includes/banner.blade.php
@@ -1,13 +1,18 @@
 
-<div id="carousel" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-    	<div class="carousel-caption d-none d-md-block">
-			<h5>TEST TEST</h5>
-		    <p>...</p>
+<div id="carousel" class="carousel custom-carousel slide" data-ride="carousel">
+  <div class="carousel-inner custom-carousel-inner">
+    <div class="carousel-item custom-item active">
+    	<div class="carousel-caption custom-carousel-caption d-none d-md-block">
+			<div class="card custom-card">
+				<div class="card-header custom-card-header">
+
+				</div>
+				<h5>test</h5>
+				<div class="card-header custom-card-header"></div>
+			</div>
 	  	</div>
 	  	<div class="overlay"></div>
-      <img class="w-100 h-50" src="{{ asset( 'images/biography.jpg' )}}" alt="First slide">
+      <img class="w-100 h-50" src="{{ asset( 'images/biography-cropped.jpg' )}}" alt="First slide">
        	
     </div>
   </div>
