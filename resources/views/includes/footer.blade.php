87ede2801e54e2bb9e583547085609d88ad53a53
John Dave Espinosa <espinosa.johndave27@gmail.com>
2018-10-19 12:24:51 +0800

    this is my first commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,117 @@
+<style>
+    
+    /*START OF FOOTER*/
+
+    .footer{
+        background-color: #1E6B57;
+
+        width: 100%;
+        
+        
+    }  
+    ul {
+      list-style-type: none;
+      padding-left:0;
+    }
+    li{
+        color: white;
+        font-size: 11pt;
+        padding: 5px 0px 5px 0;
+    }
+    .put-center{
+        text-align: center;
+        float: center;
+    }
+    .m-t-50{
+        margin-top: 50px;
+    }
+    .social-icons{
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+    .links{
+        padding-left: 15px;
+        padding-right: 15px;
+    }
+    @media (max-width: 480px){
+       .social-icons{
+        padding-left: 0px;
+        padding-right: 0px;
+        } 
+        .links{
+        padding-left: 0px;
+        padding-right: 0px;
+        } 
+    }
+    .foot-pad-bottom{
+        padding-bottom: 10vh;
+    }
+    .cust-hr{
+        background-color:#F4F4F4;
+    }
+    .cust-anchor{
+        color:#BDA940;
+        transition: all .2s linear;
+    }
+    .cust-anchor:hover{
+        color:#f4f4f4;
+        text-decoration: none;
+
+    }
+    
+
+    /**/
+</style>
+<footer class="footer">
+    <div class="container put-center" style="padding-top: 15vh; height: 20%;">
+        <div class="row">
+            <div class="col-md">
+                <strong>
+                    <a href="#" class="links cust-anchor"><font size="3">Link</font></a>
+                    <a href="#" class="links cust-anchor"><font size="3">Link</font></a>
+                    <a href="#" class="links cust-anchor"><font size="3">Link</font></a>
+                </strong>
+            </div>
+        </div>
+        <div class="row m-t-50">
+            <div class="col-md">
+
+                <a href="https://facebook.com" class="social-icons"><img src='resources/img/facebook.png'></a>
+                <a href="https://twitter.com" class="social-icons"><img src='resources/img/twitter.png'></a>
+                <a href="https://instagram.com" class="social-icons"><img src='resources/img/instagram.png'></a>
+                <a href="https://plus.google.com" class="social-icons"><img src='resources/img/google-plus.png'></a>
+                <a href="https://viber.com" class="social-icons"><img src='resources/img/viber.png'></a>
+
+            </div>
+        </div>
+        <div class="row m-t-50">
+            <div class="col-md">
+                <h5 style="color: #C8A940;">Intergalactic President Space Earth Ambassador</h5>
+                <p style="font-size: 12pt; padding-top: 0; color:white;">Zambales</p>
+
+            </div>
+        </div>
+        <!--
+        <div class="row">
+            <div class="col-md-3">
+                
+            </div>
+            <div class="col-md-3">
+               
+            </div>
+            <div class="col-md-3">
+                <h5 style="color: #C8A940;">Social Media Links</h5>
+                <a href="https://facebook.com"><img src='resources/img/facebook.png'></a>
+                <a href="https://twitter.com"><img src='resources/img/twitter.png'></a>
+                <a href="https://instagram.com"><img src='resources/img/instagram.png'></a>
+                <a href="https://plus.google.com"><img src='resources/img/google-plus.png'></a>
+                <a href="https://viber.com"><img src='resources/img/viber.png'></a>
+            </div>
+        </div>
+        -->
+        <hr class="cust-hr" />
+    </div>
+<div class="container-fluid foot-pad-bottom" style="background-color: #1E6B57; text-align: center;">
+    <h6 style="margin-bottom: 0; color:#F4F4F4;">&copy; Intergalactic President Space Earth Ambassador. All Rights Reserved</h6>
+</div>
+</footer>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html >
+<html lang="{{ app()->getLocale() }}">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="resources/admindesign/image/png" sizes="16x16" href="#">
+	<title>Intergalactic President Space Earth Ambassador</title>
+	<link rel="stylesheet" href="/css/app.css">
+	<link rel="stylesheet" href="/css/customscss.css">
+	<script type="text/javascript" src="/js/app.js"></script>
+</head>
+<body>
+@extends('includes.footer')
+@extends('includes.navbar')
+
+
+</body>
+</html>
